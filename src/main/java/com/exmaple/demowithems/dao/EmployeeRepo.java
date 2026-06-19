2264c65dd65a1be7c11ae12dbf8f837bbd40a381
nithyasreeee <nithyasreerajaram16459@gmail.com>
2026-06-19 14:48:57 +0530

    Add JWT auth, role system, Spring Security

--- src/main/java/com/exmaple/demowithems/dao/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.exmaple.demowithems.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demowithems.entity.*;
-
-
-public interface EmployeeRepo extends JpaRepository<Employee,Integer>{
-
-}
--- src/main/java/com/exmaple/demowithems/dao/ProjectRepo.java
+++
@@ -1,9 +0,0 @@
-package com.exmaple.demowithems.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demowithems.entity.*;
-public interface ProjectRepo extends JpaRepository<Project,Integer>{
-	
-
-}
